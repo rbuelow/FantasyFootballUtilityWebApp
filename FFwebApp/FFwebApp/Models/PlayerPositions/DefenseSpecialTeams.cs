e723341f5b3c8b733ac5c35cd1428ae7b58ba2d4
rbuelow <ryanbuelow@gmail.com>
2015-06-24 14:53:30 -0500

    created new asp.net project and worked on some models

---
+++ FFwebApp/FFwebApp/Models/PlayerPositions/DefenseSpecialTeams.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FFwebApp.Models.PlayerPositions
+{
+    public class DefenseSpecialTeams
+    {
+        public int KickoffReturnTDs { get; set; }
+        public int PuntReturnTDs { get; set; }
+        public int IntReturnTDs { get; set; }
+        public int FumbleRecovReturnTDs { get; set; }
+        public int Interceptions { get; set; }
+        public int FumbleRecoveries { get; set; }
+        public int BlockedKick { get; set; }
+        public int Safeties { get; set; }
+        public int Sacks { get; set; }
+        public bool ZeroPtsAllowed { get; set; }
+        public bool OneToSixPtsAllowed { get; set; }
+        public bool SevenToThirteenPtsAllowed { get; set; }
+        public bool FourteenToSeventeenPtsAllowed { get; set; }
+        public bool EighteenToTwentySevenPtsAllowed { get; set; }
+        public bool TwentyEightToThirtyFourPtsAllowed { get; set; }
+        public bool ThirtyFiveToFortyFivePtsAllowed { get; set; }      
+        public bool FortySixPlusPtsAllowed { get; set; }
+
+    }
+}
---
+++ FFwebApp/FFwebApp/Models/PlayerPositions/Individual.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FFwebApp.Models.PlayerPositions
+{
+    public class Individual
+    {
+        //Throwing stats
+        public int PassTDs { get; set; }
+        public int PassYds { get; set; }
+        public int Ints { get; set; }
+        public int TwoPtConvThrown { get; set; }
+        //Rush stats
+        public int RushYds { get; set; }
+        public int RushTDs { get; set; }
+        //Receiving stats
+        public int ReceivingYds { get; set; }
+        public int ReceivingTDs { get; set; }
+        //Misc/special teams stats
+        public int FumblesLost { get; set; }
+        public int TwoPtConvScored { get; set; }
+        public int KickoffReturnTDs { get; set; }
+        public int PuntReturnTDs { get; set; }
+        public int FumbleRecovReturnTDs { get; set; }
+        //Kicking stats
+        public int FiftyPlusFieldGoalMade { get; set; }
+        public int FortyToFortyNineFieldGoalMade { get; set; }
+        public int ZeroToThirtyNineFieldGoalMade { get; set; }
+        public int ExtraPointMade { get; set; }
+        public int FieldGoalMissed { get; set; }
+    }
+}
